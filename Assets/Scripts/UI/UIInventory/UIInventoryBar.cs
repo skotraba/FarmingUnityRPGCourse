9c9702952268437d82242337801c8cbd6b90a68b
Shannon <skotraba@gmail.com>
2021-05-26 07:59:26 -0400

    updates

---
+++ Assets/Scripts/UI/UIInventory/UIInventoryBar.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryBar : MonoBehaviour
+{
+
+    [SerializeField] private Sprite blank16Sprite = null;
+    [SerializeField] private UIInventorySlot[] inventorySlot = null;
+    public GameObject inventoryBarDraggedItem;
+
+
+    // Rect transform 
+    private RectTransform rectTransform;
+
+    private bool isInventoryBarPositionBottom = true;
+
+    //Getter and setter for inventory bar position
+    public bool IsInventoryBarPositionBottom { get => isInventoryBarPositionBottom; set => isInventoryBarPositionBottom = value; }
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // On enable method for subscribing
+    private void OnEnable()
+    {
+        EventHandler.InventoryUpdatedEvent += InventoryUpdated;
+    }
+
+    //Because we have a subscription , we also need to unsubscribe in onDisable
+    private void OnDisable()
+    {
+        EventHandler.InventoryUpdatedEvent -= InventoryUpdated;
+    }
+
+    // Clear slots so we can just rebuild them when they change
+    private void ClearInventorySlots()
+    {
+        if (inventorySlot.Length > 0)
+        {
+            for (int i = 0; i < inventorySlot.Length; i++)
+            {
+                inventorySlot[i].inventorySlotImage.sprite = blank16Sprite;
+                inventorySlot[i].textMeshProUGUI.text = "";
+                inventorySlot[i].itemDetails = null;
+                inventorySlot[i].itemQuantity = 0;
+            }
+        }
+    }
+
+    private void InventoryUpdated(InventoryLocation inventoryLocation, List<InventoryItem> inventoryList)
+    {
+
+        Debug.Log("Inventory Event");
+
+        if(inventoryLocation == InventoryLocation.player)
+        {
+            ClearInventorySlots();
+
+            if(inventorySlot.Length > 0 && inventoryList.Count > 0)
+            {
+                // loop through inventory slots and update with corresponding inventory list item
+                for (int i = 0; i < inventorySlot.Length; i++ )
+                {
+                    if(i < inventoryList.Count)
+                    {
+                        int itemCode = inventoryList[i].itemCode;
+
+                        ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(itemCode);
+
+                        if (itemDetails != null)
+                        {
+                           
+                            // add images and details to inventory item slot
+                            inventorySlot[i].inventorySlotImage.sprite = itemDetails.itemSprite;
+                            inventorySlot[i].textMeshProUGUI.text = inventoryList[i].itemQuantity.ToString();
+                            inventorySlot[i].itemDetails = itemDetails;
+                            inventorySlot[i].itemQuantity = inventoryList[i].itemQuantity;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        //Switch inventory bar position
+        SwitchInventoryBarPosition();
+    }
+
+    private void SwitchInventoryBarPosition()
+    {
+        Vector3 playerViewportPosition = Player.Instance.GetPlayerViewportPosition();
+
+        if(playerViewportPosition.y > 0.3f && IsInventoryBarPositionBottom == false)
+        {
+            rectTransform.pivot = new Vector2(0.5f, 0f);
+            rectTransform.anchorMin = new Vector2(0.5f, 0f);
+            rectTransform.anchorMax = new Vector2(0.5f, 0f);
+            rectTransform.anchoredPosition = new Vector2(0f, 2.5f);
+
+            isInventoryBarPositionBottom = true;
+        }
+        else if(playerViewportPosition.y <= 0.3f && isInventoryBarPositionBottom == true)
+        {
+            rectTransform.pivot = new Vector2(0.5f, 1f);
+            rectTransform.anchorMin = new Vector2(0.5f, 1f);
+            rectTransform.anchorMax = new Vector2(0.5f, 1f);
+            rectTransform.anchoredPosition = new Vector2(0f, -2.5f);
+
+            isInventoryBarPositionBottom = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class UIInventorySlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Camera mainCamera;
+    private Transform parentItem;
+    private GameObject draggedItem;
+
+    [SerializeField]  private UIInventoryBar inventoryBar = null;
+    [SerializeField] private GameObject itemPrefab = null;
+
+    public Image inventorySlotHighlight;
+    public Image inventorySlotImage;
+    public TextMeshProUGUI textMeshProUGUI;
+
+    [HideInInspector] public ItemDetails itemDetails;
+    [HideInInspector] public int itemQuantity;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        parentItem = GameObject.FindGameObjectWithTag(Tags.itemsParentTransform).transform;
+    }
+
+    private void DropSelectedItemAtMousePosition()
+    {
+        if (itemDetails != null)
+        {
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCamera.transform.position.z));
+
+            //Create item from prefab at mouse position
+            GameObject itemGameObject = Instantiate(itemPrefab, worldPosition, Quaternion.identity, parentItem);
+            Item item = itemGameObject.GetComponent<Item>();
+            item.ItemCode = itemDetails.itemCode;
+
+            // Remove item from players inventory
+            InventoryManager.Instance.RemoveItem(InventoryLocation.player, item.ItemCode);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        if (itemDetails != null)
+        {
+            //Disable keyboard
+            Player.Instance.DisablePlayerInputAndResetMovement();
+
+            // Instatiate gameobject as dragged item
+            draggedItem = Instantiate(inventoryBar.inventoryBarDraggedItem, inventoryBar.transform);
+
+            // Get image for dragged item
+            Image draggedItemImage = draggedItem.GetComponentInChildren<Image>();
+            draggedItemImage.sprite = inventorySlotImage.sprite;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // move game object as dragged item
+        if (draggedItem != null)
+        {
+            draggedItem.transform.position = Input.mousePosition;
+        }
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Destroy game object as dragged item
+        if (draggedItem != null)
+        {
+            Destroy(draggedItem);
+
+            //If drag ends over inventory bar, get item drag is over and swap them
+            if(eventData.pointerCurrentRaycast.gameObject != null && eventData.pointerCurrentRaycast.gameObject.GetComponent<UIInventorySlot>() != null)
+            {
+
+            }
+            else
+            {
+                if (itemDetails.canBeDropped)
+                {
+                    DropSelectedItemAtMousePosition();
+                }
+            }
+
+
+            // Enable Player input
+            Player.Instance.EnablePlayerInput();
+        }
+    }
+
+
+}
