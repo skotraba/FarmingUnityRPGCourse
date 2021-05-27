25c06fd1073b8b7a2843d8914a4f55c7146866c1
Shannon <skotraba@gmail.com>
2021-05-27 16:50:11 -0400

    updates

---
+++ Assets/Scripts/Animation/AnimationOverrides.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class AnimationOverrides : MonoBehaviour
+{
+    [SerializeField] private GameObject character = null;
+    [SerializeField] private SO_AnimationType[] soAnimationTypeArray = null;
+
+    private Dictionary<AnimationClip, SO_AnimationType> animationTypeDictionaryByAnimation;
+    private Dictionary<string, SO_AnimationType> animationTypeDictionaryByCompositeAttributeKey;
+
+
+    //****Methods****
+
+    private void Start()
+    {
+        // Initialise animation type dictionary keyed by animation clip
+        animationTypeDictionaryByAnimation = new Dictionary<AnimationClip, SO_AnimationType>();
+
+        foreach (SO_AnimationType item in soAnimationTypeArray)
+        {
+            animationTypeDictionaryByAnimation.Add(item.animationClip, item);
+        }
+
+        // Initialise animation type dictionary by string
+        animationTypeDictionaryByCompositeAttributeKey = new Dictionary<string, SO_AnimationType>();
+
+        foreach (SO_AnimationType item in soAnimationTypeArray)
+        {
+            string key = item.characterPart.ToString() + item.partVariantColor.ToString() + item.partVariantType.ToString() + item.animationName.ToString();animationTypeDictionaryByCompositeAttributeKey.Add(key, item);
+        }
+    }
+
+    public void ApplyCharacterCustomisationParameters(List<CharacterAttributes> characterAttributesList)
+    {
+        // Loop through all character and set the animation override controller for each
+        foreach (CharacterAttributes characterAttribute in characterAttributesList)
+        {
+            Animator currentAnimator = null;
+            List<KeyValuePair<AnimationClip, AnimationClip>> animsKeyValuePairList = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+
+            string animatorSOAssetName = characterAttribute.characterPart.ToString();
+
+            // Find animators in scene that match sccriptable object animator type
+            Animator[] animatorsArray = character.GetComponentsInChildren<Animator>();
+
+            foreach (Animator animator in animatorsArray)
+            {
+                if (animator.name == animatorSOAssetName)
+                {
+                    currentAnimator = animator;
+                    break;
+                }
+            }
+
+            // Get base current animations for animator
+            AnimatorOverrideController aoc = new AnimatorOverrideController(currentAnimator.runtimeAnimatorController);
+            List<AnimationClip> animationList = new List<AnimationClip>(aoc.animationClips);
+
+            foreach (AnimationClip animationClip in animationList)
+            {
+                // find animation in dictionary
+                SO_AnimationType so_AnimationType;
+                bool foundAnimation = animationTypeDictionaryByAnimation.TryGetValue(animationClip, out so_AnimationType);
+
+                if (foundAnimation)
+                {
+                    string key = characterAttribute.characterPart.ToString() + characterAttribute.partVariantColor.ToString() + characterAttribute.partVariantType.ToString() + so_AnimationType.animationName.ToString();
+
+                    SO_AnimationType swapSO_AnimationType;
+                    bool foundSwapAnimation = animationTypeDictionaryByCompositeAttributeKey.TryGetValue(key, out swapSO_AnimationType);
+
+                    if (foundSwapAnimation)
+                    {
+                        AnimationClip swapAnimationClip = swapSO_AnimationType.animationClip;
+
+                        animsKeyValuePairList.Add(new KeyValuePair<AnimationClip, AnimationClip>(animationClip, swapAnimationClip));
+                    }
+                }
+            }
+
+            //Apply animation updates to animation override controller and then update animator with the new controller
+            aoc.ApplyOverrides(animsKeyValuePairList);
+            currentAnimator.runtimeAnimatorController = aoc;
+        }
+    }
+}
---
+++ Assets/Scripts/Animation/CharacterAttributes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CharacterAttributes 
+{
+   public CharacterPartAnimator characterPart;
+   public PartVariantColor partVariantColor;
+   public PartVariantType partVariantType;
+
+   public CharacterAttributes(CharacterPartAnimator characterPart, PartVariantColor partVariantColor, PartVariantType partVariantType)
+   {
+       this.characterPart = characterPart;
+       this.partVariantColor = partVariantColor;
+       this.partVariantType = partVariantType;
+   }
+}
